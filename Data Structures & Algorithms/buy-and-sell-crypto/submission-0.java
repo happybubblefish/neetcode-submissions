9d403913f591b771e1e3ecff82ac270126e7e487
happybubblefish <congls2007@yahoo.com>
2026-04-27 20:03:40 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0, currMin = prices[0];
+
+        for(int i = 1; i < prices.length; i++) {
+            currMin = Math.min(prices[i], currMin);
+            profit = Math.max(profit, prices[i] - currMin);
+        }
+
+        return profit;
+    }
+}
