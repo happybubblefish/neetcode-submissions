a02044b96ceca522dadb46c0aae9ab18270f267f
happybubblefish <congls2007@yahoo.com>
2026-04-29 22:08:43 -0400

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+
+        int len = piles.length, left = 1, right = piles[len - 1], mid = 0;
+
+        while(left < right) {
+            mid = left + (right - left) / 2;
+
+            if(helper(piles, mid) > h) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    private int helper(int[] piles, int n) {
+        int res = 0;
+
+        for(int p : piles) {
+            res += Math.ceilDiv(p, n);
+        }
+
+        return res;
+    }
+}
