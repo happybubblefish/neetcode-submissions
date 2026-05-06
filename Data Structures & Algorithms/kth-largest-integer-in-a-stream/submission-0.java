016d8d0b3c52f2c5d98037a3e7a1342335c8a6c2
happybubblefish <congls2007@yahoo.com>
2026-05-05 21:00:16 -0400

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.java
@@ -0,0 +1,28 @@
+class KthLargest {
+
+    PriorityQueue<Integer> queue;
+    int k = 0;
+
+    public KthLargest(int k, int[] nums) {
+        queue = new PriorityQueue();
+        this.k = k;
+
+        for(int n : nums) {
+            queue.offer(n);
+
+            if(queue.size() > k) {
+                queue.poll();
+            }
+        }
+    }
+    
+    public int add(int val) {
+        queue.offer(val);
+
+        if(queue.size() > k) {
+            queue.poll();
+        }
+
+        return queue.peek();
+    }
+}
