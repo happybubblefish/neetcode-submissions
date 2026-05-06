21d8ed3036f0e90de3056a88182ffa827f29c30f
happybubblefish <congls2007@yahoo.com>
2026-05-05 20:45:21 -0400

    Add: binary-tree-maximum-path-sum - submission-2

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-2.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    int sum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        dfs(root);
+
+        return sum;
+    }
+
+    private int dfs(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int left = Math.max(dfs(root.left), 0);
+        int right = Math.max(dfs(root.right), 0);
+        int count = root.val + left + right;
+
+        sum = Math.max(sum, count);
+
+        return root.val + Math.max(left, right);
+    }
+}
