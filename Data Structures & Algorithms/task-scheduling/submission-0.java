f3dffe634b447eb1927c89e32422088ab00f407c
happybubblefish <congls2007@yahoo.com>
2026-05-05 23:08:57 -0400

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int len = tasks.length;
+
+        if(n == 0) {
+            return len;
+        }
+
+        int[] counts = new int[26];
+
+        for(char c : tasks) {
+            counts[c - 'A']++;
+        }
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int count : counts) {
+            if(count > 0) {
+                maxHeap.offer(count);
+            }
+        }
+
+        Deque<int[]> queue = new ArrayDeque<>();
+        int time = 0;
+
+        while(!maxHeap.isEmpty() || !queue.isEmpty()) {
+            time++;
+            
+            if(!maxHeap.isEmpty()) {
+                int count = maxHeap.poll() - 1;
+                if(count > 0) {
+                    queue.offer(new int[] { count, time + n });
+                }
+            }
+            
+            if(!queue.isEmpty() && queue.peek()[1] == time) {
+                maxHeap.offer(queue.poll()[0]);
+            }
+        }
+
+        return time;
+    }
+}
