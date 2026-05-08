a2d721f096e4ac35223a18e3a0d42814f2399f03
happybubblefish <congls2007@yahoo.com>
2026-05-07 21:54:15 -0400

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+
+        dfs(res, n, n, "");
+
+        return res;
+    }
+
+    private void dfs(List<String> res, int left, int right, String str) {
+        if(left < 0 || right < 0 || left > right) {
+            return;
+        }
+
+        if(left == 0 && right == 0) {
+            res.add(str);
+        }
+
+        dfs(res, left - 1, right, str + "(");
+        dfs(res, left, right - 1, str + ")");
+    }
+}
