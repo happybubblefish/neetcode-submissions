7c97295da07489eaa499b48927348d079481a8b2
happybubblefish <congls2007@yahoo.com>
2026-05-13 23:59:35 -0400

    Add: search-for-word-ii - submission-0

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-0.java
@@ -0,0 +1,71 @@
+class Solution {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String word;
+    }
+
+    TrieNode root = new TrieNode();
+
+    public List<String> findWords(char[][] board, String[] words) {
+        for(String word : words) {
+            TrieNode curr = root;
+
+            for(char c : word.toCharArray()) {
+                int index = c - 'a';
+
+                if(curr.children[index] == null) {
+                    curr.children[index] = new TrieNode();
+                }
+
+                curr = curr.children[index];
+            }
+
+            curr.word = word;
+        }
+
+        int rows = board.length, cols = board[0].length;
+        List<String> res = new ArrayList<>();
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                dfs(board, i, j, root, res);
+            }
+        }
+
+        return res;
+    }
+
+    private void dfs(char[][] board, int i, int j, TrieNode node, List<String> res) {
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length) {
+            return;
+        }
+
+        char c = board[i][j];
+        int index = c - 'a';
+
+        if(c == '#') {
+            return;
+        }
+
+        TrieNode next = node.children[index];
+
+        if(next == null) {
+            return;
+        }
+
+        if(next.word != null) {
+            res.add(next.word);
+            next.word = null;
+        }
+
+        board[i][j] = '#';
+
+        dfs(board, i - 1, j, next, res);
+        dfs(board, i + 1, j, next, res);
+        dfs(board, i, j - 1, next, res);
+        dfs(board, i, j + 1, next, res);
+
+        board[i][j] = c;
+    }
+}
