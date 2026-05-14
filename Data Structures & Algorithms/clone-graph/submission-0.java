c07e462e469a5eb53941bd7197d734fa5494a55c
happybubblefish <congls2007@yahoo.com>
2026-05-14 19:27:59 -0400

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.java
@@ -0,0 +1,51 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null) {
+            return node;
+        }
+
+        Deque<Node> queue = new LinkedList<>();
+        Map<Node, Node> map = new HashMap<>();
+
+        queue.offer(node);
+        map.put(node, new Node(node.val));
+
+        while(!queue.isEmpty()) {
+            Node n = queue.poll();
+
+            for(Node child : n.neighbors) {
+                if(map.containsKey(child)) {
+                    map.get(n).neighbors.add(map.get(child));
+                } else {
+                    Node newNode = new Node(child.val);
+                    map.put(child, newNode);
+                    queue.offer(child);
+                    map.get(n).neighbors.add(newNode);
+                }
+
+            }
+        }
+
+        return map.get(node);
+    }
+}
