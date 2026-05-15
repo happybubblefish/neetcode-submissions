307afd6fa8a243769e82bf5af28da3db55511d67
happybubblefish <congls2007@yahoo.com>
2026-05-15 14:07:38 -0400

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.java
@@ -0,0 +1,59 @@
+class WordDictionary {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isDone = false;
+    }
+
+    TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+
+        for(char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            if(curr.children[index] == null) {
+                curr.children[index] = new TrieNode();
+            }
+
+            curr = curr.children[index];
+        }
+
+        curr.isDone = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
+    private boolean dfs(String word, int i, TrieNode node) {
+        if(node == null) {
+            return false;
+        }
+
+        if(i == word.length()) {
+            return node.isDone;
+        }
+
+        char c = word.charAt(i);
+
+        if(c == '.') {
+            for(TrieNode child : node.children) {
+                if(child != null && dfs(word, i + 1, child)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        int idx = c - 'a';
+
+        return dfs(word, i + 1, node.children[idx]);
+    }
+}
