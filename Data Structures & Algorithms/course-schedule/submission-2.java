0eb7e9746d0ffef5168f7a764574cb1bbab40cbe
happybubblefish <congls2007@yahoo.com>
2026-05-17 07:03:38 -0400

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegrees = new int[numCourses];
+
+        for(int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for(int[] pre : prerequisites) {
+            int course = pre[0], prep = pre[1];
+
+            graph.get(prep).add(course);
+            indegrees[course]++;
+        }
+
+        Deque<Integer> queue = new LinkedList<>();
+
+        for(int i = 0; i < numCourses; i++) {
+            if(indegrees[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int finished = 0;
+
+        while(!queue.isEmpty()) {
+            int curr = queue.poll();
+            finished++;
+
+            for(int next : graph.get(curr)) {
+                indegrees[next]--;
+
+                if(indegrees[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+        return finished == numCourses;
+    }
+}
